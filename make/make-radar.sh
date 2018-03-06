eea3a5326cb755590f3315a0728756ca97da7f95
Boyd Shearer <boydx@users.noreply.github.com>
2018-03-06 13:15:35 -0500

    Update make-radar.sh

--- make/make-radar.sh
+++ make/make-radar.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-#Make tile set of current radar imagery for Leaflet map at https://sheltoweetrace.com/hike
+# Make tile set of current radar imagery for Leaflet map at https://sheltoweetrace.com/hike/radar.html
 
 # get jackson radar
 rm -f *.gif
 rm -f *.gfw
 rm -f *.vrt
 
+# radar gif
 wget https://radar.weather.gov/ridge/RadarImg/N0R/JKL_N0R_0.gif  
+# rader world file
 wget https://radar.weather.gov/ridge/RadarImg/N0R/JKL_N0R_0.gfw
 
+# create virtual file from GIF, define its crs, and get the transparent pixels 
 gdal_translate -a_srs EPSG:4269 -of vrt -expand rgba JKL_N0R_0.gif temp.vrt
+
+# create tileset with zoom levels 0-10. output is PNG, preserving transparent pixels
 gdal2tiles.py -z 0-10 temp.vrt jkl_radar
 
